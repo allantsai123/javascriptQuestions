a75ad39b5f0ba27bcc239b959c90ede6c26563fb
allantsai123 <allantsai123@gmail.com>
2016-08-13 21:49:11 -0700

    update files

--- isPrime.js
+++ isPrime.js
@@ -1,3 +1,22 @@
 /**
  * Created by Allan on 2016/8/13.
  */
+//How would you verify a prime number?
+
+function isPrime(n) {
+    var divisor = 2;
+
+    while(n > divisor) {
+        if (n % divisor == 0) {
+            return console.log(false);
+        } else {
+            divisor++;
+        }
+    }
+    return console.log(true);
+}
+
+for(var i =2;i<1001;i++){
+    console.log(i);
+    isPrime(i);
+}
--- primeFactor.js
+++ primeFactor.js
@@ -1,3 +1,22 @@
 /**
  * Created by Allan on 2016/8/13.
  */
+//How could you find all prime factors of a number?
+
+function primeFactor(n){
+    var divisor=2;
+    var factor=[];
+
+    while(n>2){
+        if(n%divisor==0){
+            factor.push(divisor);
+            n = n / divisor;
+        }else{
+            divisor++;
+        }
+    }
+    return console.log(factor);
+}
+
+primeFactor(36);
+//O(n)
